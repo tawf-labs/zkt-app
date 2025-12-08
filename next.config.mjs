6fbc6be44bab402520ad149467e167bc8d78b3f6
M Zidan Fatonie <77999453+mzf11125@users.noreply.github.com>
2025-12-08 09:36:56 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   images: {
     unoptimized: true,
     remotePatterns: [
